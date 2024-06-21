4349a147dfb4fa8c18f43605ecb456bc0ac8f4a2
Nhkum, Htoi <htoi.nhkum@studiosus.com>
2024-06-21 14:17:06 +0200

    composition over inherit

--- reuseable-code/stats/src/MatchReader.ts
+++ reuseable-code/stats/src/MatchReader.ts
@@ -1,6 +1,6 @@
-import { dateStringToDate } from "./utils";
-import { MatchResult } from "./MatchResult";
-type MatchData = [Date, string, string, number, number, MatchResult, string];
+import { dateStringToDate } from './utils';
+import { MatchResult } from './MatchResult';
+import { MatchData } from './MatchData';
 
 interface DataReader {
   read(): void;
@@ -14,16 +14,18 @@ export class MatchReader {
 
   load(): void {
     this.reader.read();
-    this.matches = this.reader.data.map((row: string[]): MatchData => {
-      return [
-        dateStringToDate(row[0]),
-        row[1],
-        row[2],
-        parseInt(row[3]),
-        parseInt(row[4]),
-        row[5] as MatchResult,
-        row[6],
-      ];
-    });
+    this.matches = this.reader.data.map(
+      (row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6]
+        ];
+      }
+    );
   }
 }
