ca595b39d576ca81017a778bed63fb6d4a5c6520
Nhkum, Htoi <htoi.nhkum@studiosus.com>
2024-06-24 16:20:08 +0200

    trigger event callbacks

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,7 +2,20 @@ import { User } from './models/User';
 
 const user = new User({ name: 'dev', age: 20 });
 
-user.set({ age: 34 });
+// user.set({ age: 34 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+// console.log(user.get('name'));
+// console.log(user.get('age'));
+
+user.on('change', () => {
+    console.log("Change #1")
+});
+user.on('change', () => {
+    console.log("Change #2")
+});
+user.on('save', () => {
+    console.log("Save was triggered")
+});
+
+// console.log(user);
+user.trigger('save');
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,16 +1,39 @@
 interface UserProps {
-    name?: string;
-    age?: number;
+  name?: string;
+  age?: number;
+}
+
+type Callback = () => {};
+
+export class User {
+  events: {[key: string]: Callback[]} = {};
+
+  constructor(private data: UserProps) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
   }
-  
-  export class User {
-    constructor(private data: UserProps) {}
-  
-    get(propName: string): number | string {
-      return this.data[propName];
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    //this.events[eventName] // Callback[] or undefined
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if(!handlers || handlers.length === 0){
+      return;
     }
 
-    set(update: UserProps): void {
-        Object.assign(this.data, update);
-    }
+    handlers.forEach(callback => {
+      callback();
+    })
   }
+}
