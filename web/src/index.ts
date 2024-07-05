57eb5955c23816c0e34e7e0f707481cadef4ba5f
Nhkum, Htoi <htoi.nhkum@studiosus.com>
2024-07-05 15:20:36 +0200

    binding events on class name

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,9 @@
 import { UserForm } from "./view/UserForm";
+import { User } from "./models/User";
 
-const userForm = new UserForm(document.getElementById('root'));
+const user = User.buildUser({name: "NAME", age: 21});
+
+const userForm = new UserForm(document.getElementById('root'), user);
 
 userForm.reder();
 
