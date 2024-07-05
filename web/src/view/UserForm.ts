b01d43fa1205cd23883b8abaee790d69dfaa7e89
Nhkum, Htoi <htoi.nhkum@studiosus.com>
2024-07-05 15:38:53 +0200

    re-reder on model or replace with new render template

--- web/src/view/UserForm.ts
+++ web/src/view/UserForm.ts
@@ -1,7 +1,15 @@
 import { User } from '../models/User';
 
 export class UserForm {
-  constructor(public parent: Element, public model: User) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.reder();
+    });
+  }
 
   eventsMap(): {[key: string]: ()  => void}{
     return {
@@ -9,8 +17,8 @@ export class UserForm {
     };
   }
 
-  onSetAgeClick(): void {
-    console.log('button was clicked');
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
   }
 
   template(): string {
@@ -39,6 +47,9 @@ export class UserForm {
   }
 
   reder(): void {
+    // clear parent element & replace with newly render template
+    this.parent.innerHTML = '';
+
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
 
