82fb7872e2def1319a9987785e84b2a4db11df73
Nhkum, Htoi <htoi.nhkum@studiosus.com>
2024-07-12 13:53:21 +0200

    add extracting view class

--- web/src/index.ts
+++ web/src/index.ts
@@ -7,7 +7,7 @@ const root = document.getElementById("root");
 
 if (root) {
   const userForm = new UserForm(root, user);
-  userForm.reder();
+  userForm.render();
 } else {
   throw new Error("root element not found");
 }
--- web/src/view/UserForm.ts
+++ web/src/view/UserForm.ts
@@ -1,16 +1,7 @@
 import { User } from "../models/User";
+import { View } from "./View";
 
-export class UserForm {
-  constructor(public parent: Element, public model: User) {
-    this.bindModel();
-  }
-
-  bindModel(): void {
-    this.model.on("change", () => {
-      this.reder();
-    });
-  }
-
+export class UserForm extends View {
   eventsMap(): { [key: string]: () => void } {
     return {
       "click:.set-age": this.onSetAgeClick,
@@ -19,7 +10,7 @@ export class UserForm {
   }
 
   onSetNameClick = (): void => {
-    const input = this.parent.querySelector('input');
+    const input = this.parent.querySelector("input");
 
     if (input) {
       const name = input.value;
@@ -44,28 +35,5 @@ export class UserForm {
         </div>
         `;
   }
-
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-
-    for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(":");
-
-      fragment.querySelectorAll(selector).forEach((element) => {
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      });
-    }
-  }
-
-  reder(): void {
-    // clear parent element & replace with newly render template
-    this.parent.innerHTML = "";
-
-    const templateElement = document.createElement("template");
-    templateElement.innerHTML = this.template();
-
-    this.bindEvents(templateElement.content);
-
-    this.parent.append(templateElement.content);
-  }
 }
+
