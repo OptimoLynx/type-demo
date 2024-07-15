fbc9aa02cf1f8ab0229b369635c2a1789f9a0e86
Nhkum, Htoi <htoi.nhkum@studiosus.com>
2024-07-15 13:21:12 +0200

    check login status

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from "express";
 
 interface RequestWithBody extends Request {
-    body: {[key: string]: string | undefined}
+  body: { [key: string]: string | undefined };
 }
 
 const router = Router();
@@ -22,19 +22,41 @@ router.get("/login", (req: Request, res: Response) => {
         `);
 });
 
-router.post('/login', (req:RequestWithBody, res: Response) => {
-    const { email, password} = req.body;
-    
-    if(email && password && email === 'hallo@hi.com' && password === 'password') {
-        // mark this person as loggen in
-        req.session = { loggedIn: true};
+router.post("/login", (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
 
-        // redirect them to the root route
-        res.redirect('/');
-    } else {
-        res.send('Invalid email or password.')
-    }
+  if (
+    email &&
+    password &&
+    email === "hallo@hi.com" &&
+    password === "password"
+  ) {
+    // mark this person as loggen in
+    req.session = { loggedIn: true };
+
+    // redirect them to the root route
+    res.redirect("/");
+  } else {
+    res.send("Invalid email or password.");
+  }
+});
+
+router.get("/", (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+            <div>
+                <div>You are logged in</div>
+                <a href="/logout">Logout</a>
+            </div>
+            `);
+  } else {
+    res.send(`
+            <div>
+                <div>You are not logged in</div>
+                <a href="/login">Login</a>
+            </div>        
+        `)
+  }
 });
 
 export { router };
-
