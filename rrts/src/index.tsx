b4d0da40d5bcd6dcc56d300b80cfe1152f41d6ca
Nhkum, Htoi <htoi.nhkum@studiosus.com>
2024-07-29 14:57:57 +0200

    simple  react app wix typescript

---
+++ rrts/src/index.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+interface AppProps {
+  color?: string;
+}
+
+interface AppState {
+  counter: number;
+}
+
+const el = document.getElementById("root");
+
+const root = ReactDOM.createRoot(el!);
+
+class App extends React.Component<AppProps, AppState> {
+
+  constructor(props: AppProps) {
+    super(props);
+  
+    this.state = { counter: 0 };
+  }
+
+  onIncrement = (): void => {
+    this.setState({counter: this.state.counter + 1});
+  };
+
+  onDecrement = (): void => {
+    this.setState({counter: this.state.counter - 1});
+  };
+
+  render() {
+    return (
+      <>
+        <div>Hi React wix TypeScript</div>
+        <div>{this.props.color}</div><br/>
+        <div>
+          <button onClick={this.onIncrement}>Increment</button><br/>
+          <h4>{this.state.counter}</h4>
+          <button onClick={this.onDecrement}>Decrement</button><br/>
+        </div>
+      </>
+    );
+  }
+}
+
+root.render(<App />);
+
