37fd272c2faaf0aaa867dbdb8f341be0aee96b62
Nhkum, Htoi <htoi.nhkum@studiosus.com>
2024-08-06 13:53:10 +0200

    redux setup and fix packages

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,41 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import {createStore, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from "./components/App";
+import { reducers } from "./reducers";
 
-interface AppProps {
-  color?: string;
-}
-
+/* 
 const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el!); */
 
-const root = ReactDOM.createRoot(el!);
+const store = createStore(reducers, applyMiddleware(thunk));
 
-class App extends React.Component<AppProps> {
-  state = { counter: 0 };
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+)
 
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
 
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
+ReactDOM.render(<App />, document.querySelector('#root'));
 
-  render() {
-    return (
-      <>
-        <div>Hi React wix TypeScript</div>
-        <div>{this.props.color}</div>
-        <br />
-        <div>
-          <button onClick={this.onIncrement}>Increment</button>
-          <br />
-          <h4>{this.state.counter}</h4>
-          <button onClick={this.onDecrement}>Decrement</button>
-          <br />
-        </div>
-      </>
-    );
-  }
-}
-
-root.render(<App />);
